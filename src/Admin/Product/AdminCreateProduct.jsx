e133dd7bca9c3fd72e1ea7d08f2e22d770f80f91
Prasant Singh <prasant2@amityonline.com>
2025-05-23 01:11:25 +0530

    update code

--- src/Admin/Product/AdminCreateProduct.jsx
+++ src/Admin/Product/AdminCreateProduct.jsx
@@ -129,7 +129,7 @@ export default function AdminCreateProduct() {
       );
 
       // Handle images
-      if (data.pic instanceof File) {
+      if (data.pic instanceof FileList) {
         formData.append("pic", data.pic);
       }
 
